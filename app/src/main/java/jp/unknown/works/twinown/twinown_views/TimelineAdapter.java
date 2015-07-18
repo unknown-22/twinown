14caca8dc0078071560e775d1c04f5f37f2248ea
unknown22 <sora.rr.d+unknown22@gmail.com>
2015-07-18 12:04:34 +0900

    added sortedlist

--- app/src/main/java/jp/unknown/works/twinown/twinown_views/TimelineAdapter.java
+++ app/src/main/java/jp/unknown/works/twinown/twinown_views/TimelineAdapter.java
@@ -3,7 +3,9 @@ package jp.unknown.works.twinown.twinown_views;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentManager;
+import android.support.v7.util.SortedList;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +14,6 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
-
-import java.util.ArrayList;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -29,7 +29,8 @@ class TimelineAdapter extends RecyclerView.Adapter{
     private final UserPreference userPreference;
     private RecyclerView recyclerView;
     private final LayoutInflater inflater;
-    private final ArrayList<Status> timelineList;
+    @SuppressWarnings("unchecked")
+    private final SortedList<Status> timelineList = new SortedList(Status.class, new TimelineCallback(this));
     private final RoundedTransformation transform;
 
     public TimelineAdapter(FragmentManager fragmentManager, Context context, UserPreference userPreference) {
@@ -37,17 +38,14 @@ class TimelineAdapter extends RecyclerView.Adapter{
         this.userPreference = userPreference;
         inflater = LayoutInflater.from(context);
         transform = new RoundedTransformation((int) (context.getResources().getDimension(R.dimen.icon_size) / 8));
-        timelineList = new ArrayList<>();
     }
 
     public void addStatus(Status status) {
-        timelineList.add(0, status);
-        notifyItemInserted(0);
+        timelineList.add(status);
     }
 
     public void addStatusList(ResponseList<Status> statuses) {
         timelineList.addAll(statuses);
-        notifyItemRangeInserted(timelineList.size() - statuses.size(), statuses.size());
     }
 
     @Override
@@ -106,6 +104,49 @@ class TimelineAdapter extends RecyclerView.Adapter{
                 .resizeDimen(R.dimen.icon_size, R.dimen.icon_size).transform(transform).into(statusIconView);
             statusNameView.setText(status.getUser().getScreenName());
             statusTextView.setText(status.getText());
+        }
+    }
+
+    private static class TimelineCallback extends SortedList.Callback<Status> {
+        private RecyclerView.Adapter adapter;
+
+        TimelineCallback(@NonNull RecyclerView.Adapter adapter) {
+            this.adapter = adapter;
+        }
+
+        @Override
+        public int compare(Status o1, Status o2) {
+            return o2.compareTo(o1);
+        }
+
+        @Override
+        public void onInserted(int position, int count) {
+            adapter.notifyItemRangeInserted(position, count);
+        }
+
+        @Override
+        public void onRemoved(int position, int count) {
+            adapter.notifyItemRangeInserted(position, count);
+        }
+
+        @Override
+        public void onMoved(int fromPosition, int toPosition) {
+            adapter.notifyItemMoved(fromPosition, toPosition);
+        }
+
+        @Override
+        public void onChanged(int position, int count) {
+            adapter.notifyItemRangeChanged(position, count);
+        }
+
+        @Override
+        public boolean areContentsTheSame(Status oldItem, Status newItem) {
+            return oldItem.getId() == newItem.getId();
+        }
+
+        @Override
+        public boolean areItemsTheSame(Status item1, Status item2) {
+            return item1.getId() == item2.getId();
         }
     }
 }
