2385243cc3cb73109f6962ace5f6a6ba5ad9e1c2
unknown22 <sora.rr.d+unknown22@gmail.com>
2015-07-19 01:14:31 +0900

    added tab sort key

--- app/src/main/java/jp/unknown/works/twinown/models/Tab.java
+++ app/src/main/java/jp/unknown/works/twinown/models/Tab.java
@@ -20,13 +20,18 @@ public class Tab extends Model implements Serializable {
     @Column("user_id") public Long userId;
     @Column("type") public Integer type;
     @Column("extra") public String extra;  // typeによって使う付加情報。直接参照/代入はしない。
+    @Column("sort_key") public Long sort_key;
 
     public static List<Tab> getAll() {
-        return Select.from(Tab.class).fetch();
+        return Select.from(Tab.class).orderBy("sort_key").fetch();
     }
 
     public static int getCount() {
         return Select.columns("COUNT(*)").from(Tab.class).fetchValue(Integer.class);
+    }
+
+    public static Long getMaxSortKey() {
+        return Select.columns("MAX(sort_key)").from(Tab.class).fetchValue(Long.class);
     }
 
     public static Tab createStreamTab(UserPreference userPreference) {
@@ -35,6 +40,7 @@ public class Tab extends Model implements Serializable {
         streamTab.userId = userPreference.userId;
         streamTab.type = Tab.TAB_TYPE_STREAM;
         streamTab.extra = "";
+        streamTab.sort_key = getMaxSortKey() + 1;
         streamTab.save();
         return streamTab;
     }
@@ -45,6 +51,7 @@ public class Tab extends Model implements Serializable {
         mentionTab.userId = userPreference.userId;
         mentionTab.type = Tab.TAB_TYPE_MENTION;
         mentionTab.extra = "";
+        mentionTab.sort_key = getMaxSortKey() + 1;
         mentionTab.save();
         return mentionTab;
     }
@@ -55,6 +62,7 @@ public class Tab extends Model implements Serializable {
         listTab.userId = userPreference.userId;
         listTab.type = Tab.TAB_TYPE_LIST;
         listTab.extra = String.valueOf(userList.getId());
+        listTab.sort_key = getMaxSortKey() + 1;
         listTab.save();
         return listTab;
     }
