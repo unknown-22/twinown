caf1cfa5712855b0b8b24a79fd17ff991d92de10
unknown22 <sora.rr.d+unknown22@gmail.com>
2015-09-07 23:25:53 +0900

    small fix

--- app/src/main/java/jp/unknown/works/twinown/PreviewActivity.java
+++ app/src/main/java/jp/unknown/works/twinown/PreviewActivity.java
@@ -1,10 +1,10 @@
 package jp.unknown.works.twinown;
 
 import android.content.Context;
+import android.os.Bundle;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 
